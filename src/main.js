7af89f6462661073b2163c35ae3c1f45014d38e1
blue32a <blue32a.jp@gmail.com>
2023-05-01 11:40:29 +0000

    テンプレート構文

--- src/main.js
+++ src/main.js
@@ -3,17 +3,32 @@ import { createApp } from 'vue/dist/vue.esm-bundler'
 const app = createApp({
   data() {
     return {
-      count: 0
+      msg: 'Hello Vue!!',
+      rawHtml: '<span style="color: red">This should be red.</span>',
+      dynamicId: 1374,
+      isButtonDisabled: true,
+      objectOfAttrs: {
+        id: 'bind-multiple-attributes',
+        class: 'hoge',
+      },
+      number: 1,
+      ok: true,
+      message: 'xyz',
+      id: 'hoge',
+      date: new Date(),
+      toTitleDate: (date) => {
+        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+      },
+      formatDate: (date) => {
+        return date.toString()
+      },
+      seen: true,
+      url: 'https://example.com',
+      doSomething: () => alert('click!'),
+      attributename: 'href',
+      eventname: 'click',
+      onSubmit: () => alert('submit!'),
     }
   }
 })
 app.mount('#app')
-
-const app2 = createApp({
-  data() {
-    return {
-      count: 0
-    }
-  }
-})
-app2.mount('#app2')
