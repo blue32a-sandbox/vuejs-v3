ea5c6026a6c1fb98fba891604f44e0481673872f
blue32a <blue32a.jp@gmail.com>
2023-05-01 10:28:10 +0000

    Vue アプリケーションの作成

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue/dist/vue.esm-bundler'
 
-const app = createApp()
+const app = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+})
 app.mount('#app')
+
+const app2 = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+})
+app2.mount('#app2')
